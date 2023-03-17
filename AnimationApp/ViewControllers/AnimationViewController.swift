00df4fe8d7bf541c3b0cce2de525b03f4259f45b
Dmitriy <snipere@list.ru>
2023-03-17 20:50:22 +1000

    Add outlets and action. Rename button.

---
+++ AnimationApp/ViewControllers/AnimationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AnimationViewController.swift
+//  AnimationApp
+//
+//  Created by Дмитрий Лубов on 17.03.2023.
+//
+
+import UIKit
+
+final class AnimationViewController: UIViewController {
+    
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLavel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func runButtonTapped() {
+        
+    }
+
+}
+
